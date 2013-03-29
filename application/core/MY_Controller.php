2a7b150cebf355a7d6cb595717d56ca50c8bab01
zhangxl0331 <zhangxl0331@gmail.com>
2013-03-29 17:33:47 +0800

    temporary storage

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,326 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Code here is run before ALL controllers
+ * 
+ * @package PyroCMS\Core\Controllers 
+ */
+class MY_Controller extends CI_Controller
+{
+	var $time;
+	var $onlineip;
+	var $user = array();
+	var $settings = array();
+	var $badwords = array();
+	var $applications = array();
+	var $input = array();
+
+	function __construct() {
+		$this->base();
+		$this->load->helper('install');
+		//init var
+		$this->time = time();
+		$this->onlineip = $this->input->ip_address();
+		define('FORMHASH', $this->formhash());
+		$_GET['page'] =  max(1, intval(getgpc('page')));
+		
+		$this->load->language('main');
+		
+		//init cache
+		$this->load->driver('cache');
+		
+		$cache_keys = array('settings', 'badwords', 'applications');
+		foreach($cache_keys as $v)
+		{
+			if( ! $$v = $this->cache->get($v))
+			{
+				$model = "{$v}_m";
+				$method = "get_{$v}";
+				$this->load->model($model);
+				if($$v = $this->$model->$method())
+				{
+					$this->cache->set($v, $$v);
+				}
+			}
+				
+			$this->$v = $$v;
+		}
+		
+		//init app
+		$appid = intval(getgpc('appid'));
+		
+		//init user
+		if(isset($_COOKIE['uc_auth']))
+		{
+			@list($uid, $username, $agent) = explode('|', authcode($_COOKIE['uc_auth'], 'DECODE', ($this->input ? $this->app['appauthkey'] : UC_KEY)));
+			if($agent != md5($_SERVER['HTTP_USER_AGENT']))
+			{
+				$this->setcookie('uc_auth', '');
+			}
+			else
+			{
+				@$this->user['uid'] = $uid;
+				@$this->user['username'] = $username;
+			}
+		}
+		
+		//init note
+// 		if($this->var_exists('noteexists') && !getgpc('inajax')) {
+// 			$this->load->library('notelist_l');
+// 			$this->notelist_l->send();
+// 		}
+		
+		//init mail
+// 		if($this->var_exists('mailexists') && !getgpc('inajax')) {
+// 			$this->load->library('mailqueue_l');
+// 			$this->mailqueue_l->send();
+// 		}
+		
+		//		$this->cron();
+	}
+
+	function init_input($getagent = '') {
+		$input = getgpc('input', 'R');
+		if($input) {
+			$input = $this->authcode($input, 'DECODE', $this->app['authkey']);
+			parse_str($input, $this->input);
+			$this->input = daddslashes($this->input, 1, TRUE);
+			$agent = $getagent ? $getagent : $this->input['agent'];
+
+			if(($getagent && $getagent != $this->input['agent']) || (!$getagent && md5($_SERVER['HTTP_USER_AGENT']) != $agent)) {
+				exit('Access denied for agent changed');
+			} elseif($this->time - $this->input('time') > 3600) {
+				exit('Authorization has expired');
+			}
+		}
+		if(empty($this->input)) {
+			exit('Invalid input');
+		}
+	}
+
+	function page($num, $perpage, $curpage, $mpurl) {
+		$multipage = '';
+		$mpurl .= strpos($mpurl, '?') ? '&' : '?';
+		if($num > $perpage) {
+			$page = 10;
+			$offset = 2;
+
+			$pages = @ceil($num / $perpage);
+
+			if($page > $pages) {
+				$from = 1;
+				$to = $pages;
+			} else {
+				$from = $curpage - $offset;
+				$to = $from + $page - 1;
+				if($from < 1) {
+					$to = $curpage + 1 - $from;
+					$from = 1;
+					if($to - $from < $page) {
+						$to = $page;
+					}
+				} elseif($to > $pages) {
+					$from = $pages - $page + 1;
+					$to = $pages;
+				}
+			}
+
+			$multipage = ($curpage - $offset > 1 && $pages > $page ? '<a href="'.$mpurl.'page=1" class="first"'.$ajaxtarget.'>1 ...</a>' : '').
+			($curpage > 1 && !$simple ? '<a href="'.$mpurl.'page='.($curpage - 1).'" class="prev"'.$ajaxtarget.'>&lsaquo;&lsaquo;</a>' : '');
+			for($i = $from; $i <= $to; $i++) {
+				$multipage .= $i == $curpage ? '<strong>'.$i.'</strong>' :
+				'<a href="'.$mpurl.'page='.$i.($ajaxtarget && $i == $pages && $autogoto ? '#' : '').'"'.$ajaxtarget.'>'.$i.'</a>';
+			}
+
+			$multipage .= ($curpage < $pages && !$simple ? '<a href="'.$mpurl.'page='.($curpage + 1).'" class="next"'.$ajaxtarget.'>&rsaquo;&rsaquo;</a>' : '').
+			($to < $pages ? '<a href="'.$mpurl.'page='.$pages.'" class="last"'.$ajaxtarget.'>... '.$realpages.'</a>' : '').
+			(!$simple && $pages > $page && !$ajaxtarget ? '<kbd><input type="text" name="custompage" size="3" onkeydown="if(event.keyCode==13) {window.location=\''.$mpurl.'page=\'+this.value; return false;}" /></kbd>' : '');
+
+			$multipage = $multipage ? '<div class="pages">'.(!$simple ? '<em>&nbsp;'.$num.'&nbsp;</em>' : '').$multipage.'</div>' : '';
+		}
+		return $multipage;
+	}
+
+	function page_get_start($page, $ppp, $totalnum) {
+		$totalpage = ceil($totalnum / $ppp);
+		$page =  max(1, min($totalpage, intval($page)));
+		return ($page - 1) * $ppp;
+	}
+
+	function message($message, $redirect = '', $type = 0, $vars = array()) {
+		include_once UC_ROOT.'view/default/messages.lang.php';
+		if(isset($lang[$message])) {
+			$message = $lang[$message] ? str_replace(array_keys($vars), array_values($vars), $lang[$message]) : $message;
+		}
+		$this->view->assign('message', $message);
+		$this->view->assign('redirect', $redirect);
+		if($type == 0) {
+			$this->view->display('message');
+		} elseif($type == 1) {
+			$this->view->display('message_client');
+		}
+		exit;
+	}
+
+	function formhash() {
+		return substr(md5(substr($this->time, 0, -4).UC_KEY), 16);
+	}
+
+	function submitcheck() {
+		return @getgpc('formhash', 'P') == FORMHASH ? true : false;
+	}
+
+	function date($time, $type = 3) {
+		$format[] = $type & 2 ? (!empty($this->settings['dateformat']) ? $this->settings['dateformat'] : 'Y-n-j') : '';
+		$format[] = $type & 1 ? (!empty($this->settings['timeformat']) ? $this->settings['timeformat'] : 'H:i') : '';
+		return gmdate(implode(' ', $format), $time + $this->settings['timeoffset']);
+	}
+
+	function implode($arr) {
+		return "'".implode("','", (array)$arr)."'";
+	}
+
+	function set_home($uid, $dir = '.') {
+		$uid = sprintf("%09d", $uid);
+		$dir1 = substr($uid, 0, 3);
+		$dir2 = substr($uid, 3, 2);
+		$dir3 = substr($uid, 5, 2);
+		!is_dir($dir.'/'.$dir1) && mkdir($dir.'/'.$dir1, 0777);
+		!is_dir($dir.'/'.$dir1.'/'.$dir2) && mkdir($dir.'/'.$dir1.'/'.$dir2, 0777);
+		!is_dir($dir.'/'.$dir1.'/'.$dir2.'/'.$dir3) && mkdir($dir.'/'.$dir1.'/'.$dir2.'/'.$dir3, 0777);
+	}
+
+	function get_home($uid) {
+		$uid = sprintf("%09d", $uid);
+		$dir1 = substr($uid, 0, 3);
+		$dir2 = substr($uid, 3, 2);
+		$dir3 = substr($uid, 5, 2);
+		return $dir1.'/'.$dir2.'/'.$dir3;
+	}
+
+	function get_avatar($uid, $size = 'big', $type = '') {
+		$size = in_array($size, array('big', 'middle', 'small')) ? $size : 'big';
+		$uid = abs(intval($uid));
+		$uid = sprintf("%09d", $uid);
+		$dir1 = substr($uid, 0, 3);
+		$dir2 = substr($uid, 3, 2);
+		$dir3 = substr($uid, 5, 2);
+		$typeadd = $type == 'real' ? '_real' : '';
+		return  $dir1.'/'.$dir2.'/'.$dir3.'/'.substr($uid, -2).$typeadd."_avatar_$size.jpg";
+	}
+
+	function input($k) {
+		return isset($this->input[$k]) ? (is_array($this->input[$k]) ? $this->input[$k] : trim($this->input[$k])) : NULL;
+	}
+
+	function serialize($s, $htmlon = 0) {
+		if(file_exists(UC_ROOT.RELEASE_ROOT.'./lib/xml.class.php')) {
+			include_once UC_ROOT.RELEASE_ROOT.'./lib/xml.class.php';
+		} else {
+			include_once UC_ROOT.'./lib/xml.class.php';
+		}
+
+		return xml_serialize($s, $htmlon);
+	}
+
+	function unserialize($s) {
+		if(file_exists(UC_ROOT.RELEASE_ROOT.'./lib/xml.class.php')) {
+			include_once UC_ROOT.RELEASE_ROOT.'./lib/xml.class.php';
+		} else {
+			include_once UC_ROOT.'./lib/xml.class.php';
+		}
+
+		return xml_unserialize($s);
+	}
+
+	function cutstr($string, $length, $dot = ' ...') {
+		if(strlen($string) <= $length) {
+			return $string;
+		}
+
+		$string = str_replace(array('&amp;', '&quot;', '&lt;', '&gt;'), array('&', '"', '<', '>'), $string);
+
+		$strcut = '';
+		if(strtolower(UC_CHARSET) == 'utf-8') {
+
+			$n = $tn = $noc = 0;
+			while($n < strlen($string)) {
+
+				$t = ord($string[$n]);
+				if($t == 9 || $t == 10 || (32 <= $t && $t <= 126)) {
+					$tn = 1; $n++; $noc++;
+				} elseif(194 <= $t && $t <= 223) {
+					$tn = 2; $n += 2; $noc += 2;
+				} elseif(224 <= $t && $t < 239) {
+					$tn = 3; $n += 3; $noc += 2;
+				} elseif(240 <= $t && $t <= 247) {
+					$tn = 4; $n += 4; $noc += 2;
+				} elseif(248 <= $t && $t <= 251) {
+					$tn = 5; $n += 5; $noc += 2;
+				} elseif($t == 252 || $t == 253) {
+					$tn = 6; $n += 6; $noc += 2;
+				} else {
+					$n++;
+				}
+
+				if($noc >= $length) {
+					break;
+				}
+
+			}
+			if($noc > $length) {
+				$n -= $tn;
+			}
+
+			$strcut = substr($string, 0, $n);
+
+		} else {
+			for($i = 0; $i < $length; $i++) {
+				$strcut .= ord($string[$i]) > 127 ? $string[$i].$string[++$i] : $string[$i];
+			}
+		}
+
+		$strcut = str_replace(array('&', '"', '<', '>'), array('&amp;', '&quot;', '&lt;', '&gt;'), $strcut);
+
+		return $strcut.$dot;
+	}
+
+	function setcookie($key, $value, $life = 0, $httponly = false) {
+		(!defined('UC_COOKIEPATH')) && define('UC_COOKIEPATH', '/');
+		(!defined('UC_COOKIEDOMAIN')) && define('UC_COOKIEDOMAIN', '');
+
+		if($value == '' || $life < 0) {
+			$value = '';
+			$life = -1;
+		}
+		
+		$life = $life > 0 ? $this->time + $life : ($life < 0 ? $this->time - 31536000 : 0);
+		$path = $httponly && PHP_VERSION < '5.2.0' ? UC_COOKIEPATH."; HttpOnly" : UC_COOKIEPATH;
+		$secure = $_SERVER['SERVER_PORT'] == 443 ? 1 : 0;
+		if(PHP_VERSION < '5.2.0') {
+			setcookie($key, $value, $life, $path, UC_COOKIEDOMAIN, $secure);
+		} else {
+			setcookie($key, $value, $life, $path, UC_COOKIEDOMAIN, $secure, $httponly);
+		}
+	}
+
+	function var_exists($var) 
+	{
+		$this->load->model('vars_m');
+		return $this->vars_m->var_exists($var);
+	}
+
+	function dstripslashes($string) {
+		if(is_array($string)) {
+			foreach($string as $key => $val) {
+				$string[$key] = $this->dstripslashes($val);
+			}
+		} else {
+			$string = stripslashes($string);
+		}
+		return $string;
+	}
+}
+
+/* End of file install.php */
+/* Location: application/controllers/install.php */
