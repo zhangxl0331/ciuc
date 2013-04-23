88edf809fe5f8300c0cc75ae2b29d73cd90f627c
zhangxl0331 <zhangxl0331@gmail.com>
2013-04-23 15:05:03 +0800

    modify

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -34,65 +34,7 @@ class MY_Controller extends CI_Controller
 		$this->init_mail();
 		//		$this->cron();
 		
-		$this->load->helper(array('install', 'global'));
-		//init var
-		$this->time = time();
-		$this->onlineip = $this->input->ip_address();
-		$_GET['page'] =  max(1, intval($this->input->get_post('page')));
-		
-		$this->load->language(array('main', 'templates'));
-		
-		//init cache
-		$this->load->driver('cache');
-		
-		$cache_keys = array('settings', 'badwords', 'applications');
-		foreach($cache_keys as $v)
-		{
-			if( ! $$v = $this->cache->get($v))
-			{
-				$model = "{$v}_m";
-				$method = "get_{$v}";
-				$this->load->model($model);
-				if($$v = $this->$model->$method())
-				{
-					$this->cache->save($v, $$v);
-				}
-			}
-				
-			$this->$v = $$v;
-		}
-		
-		//init app
-		$appid = intval(getgpc('appid'));
-		
-		//init user
-		if(isset($_COOKIE['uc_auth']))
-		{
-			@list($uid, $username, $agent) = explode('|', authcode($_COOKIE['uc_auth'], 'DECODE', ($this->input ? $this->app['appauthkey'] : UC_KEY)));
-			if($agent != md5($_SERVER['HTTP_USER_AGENT']))
-			{
-				$this->setcookie('uc_auth', '');
-			}
-			else
-			{
-				@$this->user['uid'] = $uid;
-				@$this->user['username'] = $username;
-			}
-		}
-		
-		//init note
-// 		if($this->var_exists('noteexists') && !getgpc('inajax')) {
-// 			$this->load->library('notelist_l');
-// 			$this->notelist_l->send();
-// 		}
-		
-		//init mail
-// 		if($this->var_exists('mailexists') && !getgpc('inajax')) {
-// 			$this->load->library('mailqueue_l');
-// 			$this->mailqueue_l->send();
-// 		}
-		
-		//		$this->cron();
+		$this->load->helper(array('global'));
 		
 		$this->sid = $this->cookie_status ? getgpc('sid', 'C') : rawurlencode(getgpc('sid', 'R'));
 		$this->load->vars('sid', $this->sid);
@@ -162,13 +104,13 @@ class MY_Controller extends CI_Controller
 	}
 	
 	function init_template() {
-		$charset = UC_CHARSET;
-		require_once UC_ROOT.'lib/template.class.php';
-		$this->view = new template();
-		$this->view->assign('dbhistories', $this->db->histories);
-		$this->view->assign('charset', $charset);
-		$this->view->assign('dbquerynum', $this->db->querynum);
-		$this->view->assign('user', $this->user);
+
+		$this->load->language('templates');
+
+		$this->load->vars('dbhistories', $this->db->queries);
+		$this->load->vars('charset', UC_CHARSET);
+		$this->load->vars('dbquerynum', $this->db->query_count);
+		$this->load->vars('user', $this->user);
 	}
 	
 	function init_note() {
@@ -320,7 +262,7 @@ class MY_Controller extends CI_Controller
 	}
 	
 	function message($message, $redirect = '', $type = 0, $vars = array()) {
-		include_once UC_ROOT.'view/default/messages.lang.php';
+		$this->load->language('message');
 		if(isset($lang[$message])) {
 			$message = $lang[$message] ? str_replace(array_keys($vars), array_values($vars), $lang[$message]) : $message;
 		}
@@ -491,7 +433,7 @@ class MY_Controller extends CI_Controller
 	}
 	
 	function note_exists() {
-		$noteexists = $this->db->fetch_first("SELECT value FROM ".UC_DBTABLEPRE."vars WHERE name='noteexists'");
+		$noteexists = $this->db->where('name', 'noteexists')->get('vars')->first_row();
 		if(empty($noteexists)) {
 			return FALSE;
 		} else {
@@ -500,7 +442,7 @@ class MY_Controller extends CI_Controller
 	}
 	
 	function mail_exists() {
-		$mailexists = $this->db->fetch_first("SELECT value FROM ".UC_DBTABLEPRE."vars WHERE name='mailexists'");
+		$mailexists = $this->db->where('name', 'mailexists')->get('vars')->first_row();
 		if(empty($mailexists)) {
 			return FALSE;
 		} else {
@@ -605,172 +547,6 @@ class MY_Controller extends CI_Controller
 		$authkey = md5($ip.$agent.UC_KEY);
 		$check = substr(md5($ip.$agent), 0, 8);
 		return rawurlencode(authcode("$username\t$check", 'ENCODE', $authkey, 1800));
-	}
-
-	function message($message, $redirect = '', $type = 0, $vars = array()) {
-		$this->load->language('message');
-		if(isset($lang[$message])) {
-			$message = $lang[$message] ? str_replace(array_keys($vars), array_values($vars), $lang[$message]) : $message;
-		}
-		$this->view->assign('message', $message);
-		$this->view->assign('redirect', $redirect);
-		if($type == 0) {
-			$this->view->display('message');
-		} elseif($type == 1) {
-			$this->view->display('message_client');
-		}
-		exit;
-	}
-
-	function date($time, $type = 3) {
-		$format[] = $type & 2 ? (!empty($this->settings['dateformat']) ? $this->settings['dateformat'] : 'Y-n-j') : '';
-		$format[] = $type & 1 ? (!empty($this->settings['timeformat']) ? $this->settings['timeformat'] : 'H:i') : '';
-		return gmdate(implode(' ', $format), $time + $this->settings['timeoffset']);
-	}
-
-	function implode($arr) {
-		return "'".implode("','", (array)$arr)."'";
-	}
-
-	function set_home($uid, $dir = '.') {
-		$uid = sprintf("%09d", $uid);
-		$dir1 = substr($uid, 0, 3);
-		$dir2 = substr($uid, 3, 2);
-		$dir3 = substr($uid, 5, 2);
-		!is_dir($dir.'/'.$dir1) && mkdir($dir.'/'.$dir1, 0777);
-		!is_dir($dir.'/'.$dir1.'/'.$dir2) && mkdir($dir.'/'.$dir1.'/'.$dir2, 0777);
-		!is_dir($dir.'/'.$dir1.'/'.$dir2.'/'.$dir3) && mkdir($dir.'/'.$dir1.'/'.$dir2.'/'.$dir3, 0777);
-	}
-
-	function get_home($uid) {
-		$uid = sprintf("%09d", $uid);
-		$dir1 = substr($uid, 0, 3);
-		$dir2 = substr($uid, 3, 2);
-		$dir3 = substr($uid, 5, 2);
-		return $dir1.'/'.$dir2.'/'.$dir3;
-	}
-
-	function get_avatar($uid, $size = 'big', $type = '') {
-		$size = in_array($size, array('big', 'middle', 'small')) ? $size : 'big';
-		$uid = abs(intval($uid));
-		$uid = sprintf("%09d", $uid);
-		$dir1 = substr($uid, 0, 3);
-		$dir2 = substr($uid, 3, 2);
-		$dir3 = substr($uid, 5, 2);
-		$typeadd = $type == 'real' ? '_real' : '';
-		return  $dir1.'/'.$dir2.'/'.$dir3.'/'.substr($uid, -2).$typeadd."_avatar_$size.jpg";
-	}
-
-	function input($k) {
-		return isset($this->input[$k]) ? (is_array($this->input[$k]) ? $this->input[$k] : trim($this->input[$k])) : NULL;
-	}
-
-	function serialize($s, $htmlon = 0) {
-		if(file_exists(FCPATH.RELEASE_ROOT.'./lib/xml.class.php')) {
-			include_once FCPATH.RELEASE_ROOT.'./lib/xml.class.php';
-		} else {
-			include_once FCPATH.'./lib/xml.class.php';
-		}
-
-		return xml_serialize($s, $htmlon);
-	}
-
-	function unserialize($s) {
-		if(file_exists(FCPATH.RELEASE_ROOT.'./lib/xml.class.php')) {
-			include_once FCPATH.RELEASE_ROOT.'./lib/xml.class.php';
-		} else {
-			include_once FCPATH.'./lib/xml.class.php';
-		}
-
-		return xml_unserialize($s);
-	}
-
-	function cutstr($string, $length, $dot = ' ...') {
-		if(strlen($string) <= $length) {
-			return $string;
-		}
-
-		$string = str_replace(array('&amp;', '&quot;', '&lt;', '&gt;'), array('&', '"', '<', '>'), $string);
-
-		$strcut = '';
-		if(strtolower(UC_CHARSET) == 'utf-8') {
-
-			$n = $tn = $noc = 0;
-			while($n < strlen($string)) {
-
-				$t = ord($string[$n]);
-				if($t == 9 || $t == 10 || (32 <= $t && $t <= 126)) {
-					$tn = 1; $n++; $noc++;
-				} elseif(194 <= $t && $t <= 223) {
-					$tn = 2; $n += 2; $noc += 2;
-				} elseif(224 <= $t && $t < 239) {
-					$tn = 3; $n += 3; $noc += 2;
-				} elseif(240 <= $t && $t <= 247) {
-					$tn = 4; $n += 4; $noc += 2;
-				} elseif(248 <= $t && $t <= 251) {
-					$tn = 5; $n += 5; $noc += 2;
-				} elseif($t == 252 || $t == 253) {
-					$tn = 6; $n += 6; $noc += 2;
-				} else {
-					$n++;
-				}
-
-				if($noc >= $length) {
-					break;
-				}
-
-			}
-			if($noc > $length) {
-				$n -= $tn;
-			}
-
-			$strcut = substr($string, 0, $n);
-
-		} else {
-			for($i = 0; $i < $length; $i++) {
-				$strcut .= ord($string[$i]) > 127 ? $string[$i].$string[++$i] : $string[$i];
-			}
-		}
-
-		$strcut = str_replace(array('&', '"', '<', '>'), array('&amp;', '&quot;', '&lt;', '&gt;'), $strcut);
-
-		return $strcut.$dot;
-	}
-
-	function setcookie($key, $value, $life = 0, $httponly = false) {
-		(!defined('UC_COOKIEPATH')) && define('UC_COOKIEPATH', '/');
-		(!defined('UC_COOKIEDOMAIN')) && define('UC_COOKIEDOMAIN', '');
-
-		if($value == '' || $life < 0) {
-			$value = '';
-			$life = -1;
-		}
-		
-		$life = $life > 0 ? $this->time + $life : ($life < 0 ? $this->time - 31536000 : 0);
-		$path = $httponly && PHP_VERSION < '5.2.0' ? UC_COOKIEPATH."; HttpOnly" : UC_COOKIEPATH;
-		$secure = $_SERVER['SERVER_PORT'] == 443 ? 1 : 0;
-		if(PHP_VERSION < '5.2.0') {
-			setcookie($key, $value, $life, $path, UC_COOKIEDOMAIN, $secure);
-		} else {
-			setcookie($key, $value, $life, $path, UC_COOKIEDOMAIN, $secure, $httponly);
-		}
-	}
-
-	function var_exists($var) 
-	{
-		$this->load->model('vars_m');
-		return $this->vars_m->var_exists($var);
-	}
-
-	function dstripslashes($string) {
-		if(is_array($string)) {
-			foreach($string as $key => $val) {
-				$string[$key] = $this->dstripslashes($val);
-			}
-		} else {
-			$string = stripslashes($string);
-		}
-		return $string;
 	}
 }
 
--- application/helpers/global_helper.php
+++ application/helpers/global_helper.php
@@ -19,3 +19,56 @@ function getgpc($k, $t='GP') {
 	}
 	return isset($var[$k]) ? $var[$k] : '';
 }
+function authcode($string, $operation = 'DECODE', $key = '', $expiry = 0) {
+
+	$ckey_length = 4;	// �����Կ���� ȡֵ 0-32;
+	// ���������Կ���������������κι��ɣ�������ԭ�ĺ���Կ��ȫ��ͬ�����ܽ��Ҳ��ÿ�β�ͬ�������ƽ��Ѷȡ�
+	// ȡֵԽ�����ı䶯����Խ�����ı仯 = 16 �� $ckey_length �η�
+	// ����ֵΪ 0 ʱ���򲻲��������Կ
+
+	$key = md5($key ? $key : UC_KEY);
+	$keya = md5(substr($key, 0, 16));
+	$keyb = md5(substr($key, 16, 16));
+	$keyc = $ckey_length ? ($operation == 'DECODE' ? substr($string, 0, $ckey_length): substr(md5(microtime()), -$ckey_length)) : '';
+
+			$cryptkey = $keya.md5($keya.$keyc);
+			$key_length = strlen($cryptkey);
+
+			$string = $operation == 'DECODE' ? base64_decode(substr($string, $ckey_length)) : sprintf('%010d', $expiry ? $expiry + time() : 0).substr(md5($string.$keyb), 0, 16).$string;
+			$string_length = strlen($string);
+
+			$result = '';
+			$box = range(0, 255);
+
+			$rndkey = array();
+			for($i = 0; $i <= 255; $i++) {
+				$rndkey[$i] = ord($cryptkey[$i % $key_length]);
+			}
+
+			for($j = $i = 0; $i < 256; $i++) {
+				$j = ($j + $box[$i] + $rndkey[$i]) % 256;
+				$tmp = $box[$i];
+				$box[$i] = $box[$j];
+				$box[$j] = $tmp;
+}
+
+for($a = $j = $i = 0; $i < $string_length; $i++) {
+	$a = ($a + 1) % 256;
+	$j = ($j + $box[$a]) % 256;
+	$tmp = $box[$a];
+	$box[$a] = $box[$j];
+	$box[$j] = $tmp;
+	$result .= chr(ord($string[$i]) ^ ($box[($box[$a] + $box[$j]) % 256]));
+}
+
+	if($operation == 'DECODE') {
+	if((substr($result, 0, 10) == 0 || substr($result, 0, 10) - time() > 0) && substr($result, 10, 16) == substr(md5(substr($result, 26).$keyb), 0, 16)) {
+	return substr($result, 26);
+	} else {
+	return '';
+	}
+	} else {
+		return $keyc.str_replace('=', '', base64_encode($result));
+		}
+
+		}
