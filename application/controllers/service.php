1a8d1c5a106cab4c15c67316c07e86d9118b5712
zhangxl0331 <zhangxl0331@gmail.com>
2013-04-26 17:32:25 +0800

    modify

---
+++ application/controllers/service.php
@@ -0,0 +1,81 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Service extends MY_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+
+	}
+	
+	function seccode() {
+		$authkey = md5(UC_KEY.$_SERVER['HTTP_USER_AGENT'].$this->onlineip);
+
+		$this->time = time();
+		$seccodeauth = $this->input->get_post('seccodeauth');
+		$seccode = authcode($seccodeauth, 'DECODE', $authkey);
+		
+		//$seccode = rand(100000, 999999);
+		//$this->setcookie('uc_secc', $this->authcode($seccode."\t".$this->time, 'ENCODE'));
+
+		@header("Expires: -1");
+		@header("Cache-Control: no-store, private, post-check=0, pre-check=0, max-age=0", FALSE);
+		@header("Pragma: no-cache");
+
+		$this->load->library('seccode');
+		$this->seccode->code = $seccode;
+		$this->seccode->type = 0;
+		$this->seccode->width = 70;
+		$this->seccode->height = 21;
+		$this->seccode->background = 0;
+		$this->seccode->adulterate = 1;
+		$this->seccode->ttf = 1;
+		$this->seccode->angle = 0;
+		$this->seccode->color = 1;
+		$this->seccode->size = 0;
+		$this->seccode->shadow = 1;
+		$this->seccode->animator = 0;
+		$this->seccode->fontpath = FCPATH.'images/fonts/';
+		$this->seccode->datapath = FCPATH.'images/';
+		$this->seccode->includepath = '';
+		$this->seccode->display();
+	}
+	
+	function avatar()
+	{
+		$uid = isset($_GET['uid']) ? $_GET['uid'] : 0;
+		$size = isset($_GET['size']) ? $_GET['size'] : '';
+		$random = isset($_GET['random']) ? $_GET['random'] : '';
+		$type = isset($_GET['type']) ? $_GET['type'] : '';
+		$check = isset($_GET['check_file_exists']) ? $_GET['check_file_exists'] : '';
+		
+		$avatar = 'data/avatar/'.get_avatar($uid, $size, $type);
+		if(file_exists(FCPATH.$avatar)) {
+			if($check) {
+				echo 1;
+				exit;
+			}
+			$random = !empty($random) ? rand(1000, 9999) : '';
+			$avatar_url = empty($random) ? $avatar : $avatar.'?random='.$random;
+		} else {
+			if($check) {
+				echo 0;
+				exit;
+			}
+			$size = in_array($size, array('big', 'middle', 'small')) ? $size : 'middle';
+			$avatar_url = 'images/noavatar_'.$size.'.gif';
+		}
+		
+		if(empty($random)) {
+			header("HTTP/1.1 301 Moved Permanently");
+			header("Last-Modified:".date('r'));
+			header("Expires: ".date('r', time() + 86400));
+		}
+		
+		header('Location: '.$this->config->base_url($avatar_url));
+		exit;
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
